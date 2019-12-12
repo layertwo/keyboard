92506f4ac75b6d74149468a42d9492fbc70269b0
Lucas Messenger <lucas.c.messenger@gmail.com>
2019-12-11 20:16:20 -0500

    Add tap dance, multimedia control, cleanup layout

--- lily58/keymap.c
+++ lily58/keymap.c
@@ -13,10 +13,19 @@
 #define _RMOD 2
 #define _FMOD 3
 
+enum custom_keycodes {
+    TD_LOCK = 0 // tap dance macro
+};
+
 #ifdef RGBLIGHT_ENABLE
 //Following line allows macro to read current RGB settings
 extern rgblight_config_t rgblight_config;
 #endif
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // right control if held or tap once, lock macOS if double tap */
+    [TD_LOCK] = ACTION_TAP_DANCE_DOUBLE(TO(_RMOD), LCTL(LCMD(KC_Q)))
+};
 
 extern uint8_t is_master;
 
@@ -24,9 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_BASE] = LAYOUT( \
   KC_GESC,   KC_1,   KC_2,   KC_3,     KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC, \
   KC_TAB,    KC_Q,   KC_W,   KC_E,     KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS, \
-  KC_CAPS,   KC_A,   KC_S,   KC_D,     KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
-  KC_LSFT,   KC_Z,   KC_X,   KC_C,     KC_V,    KC_B, LT(2,KC_LBRC), LT(1,KC_RBRC), KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, \
-                             KC_LCTL,  KC_LALT, KC_LGUI, KC_SPC,            KC_ENT, KC_DEL,  KC_MINS, KC_EQL \
+  KC_LCTL,   KC_A,   KC_S,   KC_D,     KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
+  KC_LSFT,   KC_Z,   KC_X,   KC_C,     KC_V,    KC_B, LT(_RMOD,KC_LBRC), LT(_LMOD,KC_RBRC), KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, \
+                             TD(TD_LOCK),  KC_LALT, KC_LGUI, KC_SPC,            KC_ENT, KC_DEL,  KC_MINS, KC_EQL \
 ),
  [_LMOD] = LAYOUT( \
   KC_NO,     KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO, \
@@ -36,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_NO,    KC_NO,   KC_NO,    KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO \
 ), //TODO m_ent
  [_RMOD] = LAYOUT( \
-  KC_NO,     KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_NO, \
+  KC_NO,     KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_NO, \
   KC_NO,     KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_MRWD,  KC_MFFD,  KC_MPLY,  KC_NO, \
   KC_NO,     KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,                              KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT,  KC_NO,    KC_NO, \
   KC_NO,     KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,        KC_NO,  KC_NO,        KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO, \
@@ -92,8 +101,6 @@ void matrix_render_user(struct CharacterMatrix *matrix) {
   if (is_master) {
     // If you want to change the display of OLED, you need to change here
     matrix_write_ln(matrix, read_layer_state());
-    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-    //matrix_write_ln(matrix, read_host_led_state());
     matrix_write_ln(matrix, read_timelog());
   } else {
     matrix_write(matrix, read_logo());
