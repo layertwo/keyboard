031f4a0025f26dbf800d2441084209436c158929
Lucas Messenger <lucas.c.messenger@gmail.com>
2020-07-18 23:46:29 -0400

    Fix lily58 to build with latest QMK

--- lily58/rules.mk
+++ lily58/rules.mk
@@ -14,15 +14,15 @@ AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
-SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 TAP_DANCE_ENABLE = yes      # Enable tap dance
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+OLED_DRIVER_ENABLE = yes    # OLED display
+SPLIT_KEYBOARD = yes        # Is a split keyboard
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/glcdfont.c \
-        ./lib/timelogger.c \
+SRC +=  ./lib/timelogger.c \
         ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c
