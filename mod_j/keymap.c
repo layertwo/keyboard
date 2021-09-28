d76fb2f3d33fdf1b37d874c2a551fd74e268c55e
Lucas Messenger <lucas.c.messenger@gmail.com>
2021-09-27 21:04:43 -0700

    Add support for delete

--- mod_j/keymap.c
+++ mod_j/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                          KC_NO, KC_NO, KC_NO, KC_SPC, KC_NO, TO(0), KC_NO, KC_NO),
 
     // system control layer
-    [2] = LAYOUT_60_ansi_split_bs_rshift(KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO,
+    [2] = LAYOUT_60_ansi_split_bs_rshift(KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_MUTE, KC_VOLD, KC_VOLU, KC_DEL, KC_NO,
                                          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPRV, KC_MNXT, KC_NO,
                                          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, BL_BRTG, KC_NO,
                                          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, BL_DEC, BL_INC, BL_TOGG, KC_NO, KC_NO,
